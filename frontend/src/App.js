0e7008ff6a17ecce2317a0f1cc28e8c5ad878092
Jung-suk <mohamedabhuthahirkhan.19cse@sonatech.ac.in>
2023-02-01 13:05:05 +0530

    First Commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,18 @@
+import { Container } from 'react-bootstrap'
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import './bootstrap.min.css'
 
 const App = () => {
   return (
     <>
-      <p>Welcome!!!</p>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <h1>Welcome!!!</h1>
+        </Container>
+      </main>
+      <Footer />
     </>
   );
 }
